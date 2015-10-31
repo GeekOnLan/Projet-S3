00a7847f03917fda4e4bc3b89df08b77c69d0eb6
pamplemousse26 <romains.manciaux@laposte.net>
2015-10-31 11:21:38 +0100

    Ajout des classes Member et MyPdo

--- classes/Member.class.php
+++ classes/Member.class.php
@@ -1,50 +1,85 @@
-﻿<?php 
+<?php 
 
+class Member{
 
+	/*
+	* id du membre
+	*/
+	private $idMembre = null;
 
- class Member{
+	/*
+	* nom du membre
+	*/
+	private $nom = null;
 
- 	/*
- 	* id du membre
- 	*/
- 	private $idMembre = null;
+	/*
+	* prenom du membre
+	*/
+	private $prenom = null;
 
- 	/*
- 	* nom du membre
- 	*/
- 	private $nom = null;
+	/*
+	* pseudo du membre
+	*/
+	private $pseudo = null;
 
- 	/*
- 	* prenom du membre
- 	*/
- 	private $prenom = null;
+	/*
+	* mail du membre
+	*/
+	private $mail = null;
 
- 	/*
- 	* pseudo du membre
- 	*/
- 	private $pseudo = null;
+	/*
+	* dateNais du membre
+	*/
+	private $dateNais = null;
 
- 	/*
- 	* mail du membre
- 	*/
- 	private $mail = null;
+	// getter
 
- 	/*
- 	* dateNais du membre
- 	*/
- 	private $dateNais = null;
+	/*
+	* retourne l'id du membre
+	*/
+	public function getId(){
+		return $this->id;
+	}
 
- 	
+	/*
+	* retourne le nom
+	*/
+	public function getLastName(){
+		return $this->nom;
+	}
 
- 	private function __construct(){}
+	/*
+	* retourne le prenom
+	*/
+	public function getFirstName(){
+		return $this->prenom;
+	}
 
- 	 /**
-	 * Usine pour fabriquer une instance à partir d'un identifiant
+	/*
+	* retourne le mail
+	*/
+	public function getMail(){
+		return $this->mail;
+	}
+
+	/*
+	* retourne la date de naissance
+	*/
+	public function getBirthday(){
+		return $this->dateNais;
+	}
+
+	/*
+	* pour ne pas cree d`instance de membre sans id
+	*/
+	private function __construct(){}
+
+	 /**
+	 * Usine pour fabriquer une instance à partir d'un pseudo et un mot de passe
 	 * Les données sont issues de la base de données
-	 * @param int $id identifiant BD du membre à créer
-	 * @return Member instance correspondant à $id
 	 */
 	public static function createFromAuth($pseudo,$mdp) {
+		myPDO::setConfiguration('mysql:host=localhost;dbname=geekOnLAN;charset=utf8', 'root', '');
 		$pdo = myPDO::GetInstance();
 		$stmt = $pdo->prepare(<<<SQL
 			SELECT *
@@ -52,50 +87,57 @@
 			WHERE pseudo = ? AND pass = ?;
 SQL
 		);
-
-		
 		$stmt->execute(array($pseudo,SHA1($mdp)));
 		$stmt->setFetchMode(PDO::FETCH_CLASS, __CLASS__);
 		$member = $stmt->fetch();
 		if($member!==false){
 			self::startSession();
-			$_SESSION['connected']=true;
-			
 			return $member;
 		}
 		else{
-			throw new Exception("Pseudo/Mot de passe invalide");
-		}				  
- 	}
- 		
+			throw new Exception("Pseudo ou mot de passe invalide");
+		}		  
+	}
+		
+	/*
+	* demarre une session si elle n'est pas deja demarrer
+	*/
+	private static function startSession(){
+		if(session_status()==PHP_SESSION_NONE){
+			if(session_status()==PHP_SESSION_ACTIVE)throw new Exception('erreur lancement de session');
+			else session_start();
+		}
+	}
 
+	/*
+	* return true si le membre et connecter false sinon
+	*/
+	public static function isConnected(){
+		self::startSession();
+		if(isset($_SESSION['Member']) && !empty($_SESSION['Member'])){
+			return true;
+		}
+		else return false;
+	}
 
- 	/*
- 	* demmare une session si elle n'est pas deja demmarer
- 	*/
- 	private static function startSession(){
- 		if(session_status()==PHP_SESSION_NONE){
- 			if(session_status()==PHP_SESSION_ACTIVE)throw new Exception('erreur lancement de session');
- 			else session_start();
- 		}
- 	}
+	/*
+	* stock le membre dans la session
+	*/
+	public function saveIntoSession(){
+		self::startSession();
+		$_SESSION['Member']=$this;
+	}
 
- 	/*
- 	* return true si le membre et connecter false sinon
- 	*/
- 	public static function isConnected(){
- 		self::startSession();
- 		if(isset($_SESSION['connected'])){
- 			return $_SESSION['connected'];
- 		}
- 		else return false;
- 	}
+	public static function disconnect(){
+		self::startSession();
+		$_SESSION['Member']=null;
+	}
 
- 	/*
- 	* stock le membre dans la session
- 	*/
- 	public function saveIntoSession(){
- 		self::startSession();
- 		$_SESSION['Member']=$this;
- 	}
- }
+	public static function GetInstance(){
+		self::startSession();
+		if(self::isConnected())
+			return $_SESSION['Member'];
+		else
+			return null;
+	}
+}
