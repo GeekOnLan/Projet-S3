ed6be2ae2ae78f42fa3825d134df198fa8de22bb
TCExterminator <TCExterminator@users.noreply.github.com>
2015-11-06 15:41:01 +0100

    creation sendMailValid
    script d'envoie des mail de confirmation

---
+++ sendMailValid.php
@@ -0,0 +1,36 @@
+<?php
+//w
+// Préparation du mail contenant le lien d'activation
+function envoieMailValide($login,$email){
+	//génération aléatoire d'une clé
+	$key = md5(microtime(TRUE)*100000);
+
+	// Insertion de la clé dans la base de données
+	$stmt = myPDO::GetInstance();
+	$stmt = $dbh->prepare("UPDATE Membre SET cleMail=:key WHERE pseudo like :login");
+	$stmt->bindParam(':key', $key);
+	$stmt->bindParam(':login', $login);
+	$stmt->execute();
+
+ 	$destinataire = $email;
+	$sujet = "Activation de votre compte sur geekonlan" ;
+	$entete = "From: inscription@geekonlan.com" ;
+	$key = urlencode($key);
+	$login = urlencode($login);
+	// Le lien d'activation est composé du login(login) et de la clé(key)
+	$message =<<<HTML
+Bienvenue sur GeekOnLAN,
+
+Pour activer votre compte, veuillez cliquer sur le lien ci dessous
+ou le copier/coller dans votre navigateur internet.
+
+http://geekonlancom/activation.php?log={$login}&key={$key}
+
+
+---------------
+Ceci est un mail automatique, Merci de ne pas y répondre
+
+HTML;
+
+	mail($destinataire, $sujet, $message, $entete) ; // Envoi du mail
+}
