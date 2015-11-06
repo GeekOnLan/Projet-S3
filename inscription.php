daa2f18cf33a1be14f53a2af91e92ef9d9aa1942
TCExterminator <TCExterminator@users.noreply.github.com>
2015-11-06 15:45:38 +0100

    rajout de la fonction envoie Mail
    envoie du mail

--- inscription.php
+++ inscription.php
@@ -38,6 +38,7 @@ SQL
         "password"=>$password,
         "mail"=>$mail,
         "birthday"=>$bD));
+    envoieMailValide($pseudo,$mail);
     $form->appendContent("<p>Vous &#234;tes bien inscrit ! Vous allez recevoir un email de confirmation</p>");
 }
 
@@ -68,6 +69,39 @@ HTML;
 	return $html;
 	
 }
+// Préparation du mail contenant le lien d'activation
+function envoieMailValide($login,$email){
+	//génération aléatoire d'une clé
+	$key = md5(microtime(TRUE)*100000);
 
+	// Insertion de la clé dans la base de données
+	$stmt = myPDO::GetInstance();
+	$stmt = $dbh->prepare("UPDATE Membre SET cleMail=:key WHERE pseudo like :login");
+	$stmt->bindParam(':key', $key);
+	$stmt->bindParam(':login', $login);
+	$stmt->execute();
+
+ 	$destinataire = $email;
+	$sujet = "Activation de votre compte sur geekonlan" ;
+	$entete = "From: inscription@geekonlan.com" ;
+	$key = urlencode($key);
+	$login = urlencode($login);
+	// Le lien d'activation est composé du login(login) et de la clé(key)
+	$message =<<<HTML
+Bienvenue sur GeekOnLAN,
+
+Pour activer votre compte, veuillez cliquer sur le lien ci dessous
+ou le copier/coller dans votre navigateur internet.
+
+http://geekonlancom/activation.php?log={$login}&key={$key}
+
+
+---------------
+Ceci est un mail automatique, Merci de ne pas y répondre
+
+HTML;
+
+	mail($destinataire, $sujet, $message, $entete) ; // Envoi du mail
+}
 
 
