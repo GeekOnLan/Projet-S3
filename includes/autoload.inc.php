2faf1359092388ddf53d2b989aab6cbf1353e3b4
TCExterminator <TCExterminator@users.noreply.github.com>
2015-11-10 08:24:32 +0100

    Update autoload.inc.php

--- includes/autoload.inc.php
+++ includes/autoload.inc.php
@@ -6,7 +6,7 @@
  * @throws Exception
  */
 function __autoload($name){
-	$fichier = $_SERVER['DOCUMENT_ROOT'].'/Projet-S3/classes/'.$name.'.class.php';
+	$fichier = $_SERVER['DOCUMENT_ROOT'].'/classes/'.$name.'.class.php';
     if(file_exists($fichier))
         require_once($fichier);
     else throw new Exception($fichier." introuvable");
