4b78e63f6378d17eab6ab63583a564a5bf63a35b
PandarkMeow <damneddu10@hotmail.fr>
2015-11-12 17:29:11 +0100

    ajout d'un plugin pour le parallax, Marche toujours pas

--- classes/GeekOnLanWebpage.class.php
+++ classes/GeekOnLanWebpage.class.php
@@ -61,6 +61,9 @@ class GeekOnLanWebpage extends Webpage {
         $this->appendCssUrl("style/mobile/base.css", "screen and (max-width: 680px)");
         $this->appendJsUrl("http://code.jquery.com/jquery-2.1.4.min.js");
         $this->appendJsUrl("js/base.js");
+        $this->appendJsUrl("js/parallaxPlugin/jquery.simple3D.js");
+        $this->appendJsUrl("js/parallax.js");
+        $this->appendCssUrl("style/parallax.css");
         if(!Member::isConnected()) {
             $this->appendJsUrl("js/authentification.js");
             $this->appendJsUrl("http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha256.js");
@@ -140,6 +143,11 @@ HTML;
                 $auth
             </ul>
         </nav>
+        <div id="parallax">
+        <div class="bottom"></div>
+        <div class="middle"></div>
+        <div class="front"></div>
+        </div>
 HTML
         );
     }
--- js/parallax.js
+++ js/parallax.js
@@ -1,34 +1,11 @@
 /**
  * Created by PandarkMeow on 04/11/2015.
  */
-$(document).ready(function() {
-    alert($('#parallax').length);
-       $('#parallax').mousemove(
-        function(e){
-            //alert("coucou");
-            /*Position souris*/
-            var offset = $(this).offset();
-            var posX = e.pageX - offset.left;
-            var posY = e.pageY - offset.top;
-
-            /*Position en pourcentage*/
-            var mouseX = Math.round(posX / $(this).width() * 100);
-            var mouseY = Math.round(posY / $(this).height() * 100);
-
-            /*Position de chaques couches*/
-            $(this).children('img').each(
-                function(){
-                    //alert("coucou");
-                    var myX =  ($('#parallax').width() - $(this).width()) * (mouseX / 100);
-                    var myY =  ($('#parallax').height() - $(this).height()) * (mouseY / 100);
-
-                    var cssObj = {
-                        'left' : myX + 'px',
-                        'top' : myY + 'px'
-                    };
-                    $(this).animate({left: myX, top: myY}, {duration: 50, queue: false, easing : 'linear'});
-                }
-            );
-        }
-    );
+$("#parallax").simple3D({
+    moveX:3, // 1 - 5
+    moveY:3, // 1 - 5
+    bgImage:true, // use background image mode
+    targetAll:true,
+    reverseX: true,
+    reverseY: true
 });
