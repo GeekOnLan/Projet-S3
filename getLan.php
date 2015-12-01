71dd78c05ff79403bb823e84be3d42124877d6ae
Nicolas Mezenen <nicolas.mezenen@etudiant.univ-reims.fr>
2015-12-01 14:45:13 +0100

    Coucou je te mange avec de la sauce tandoori

--- getLan.php
+++ getLan.php
@@ -2,32 +2,27 @@
 require_once("includes/utility.inc.php");
 require_once("includes/autoload.inc.php");
 
-$xml = "<?xml version = \"1.0\" encoding=\"UTF-8\"?>";
-
 if(verify($_GET,'idLan')){
-	header('Content-Type: application/xml');
+//	header('Content-Type: application/json');
 	usleep(rand(0, 20) * 100000);
 	$lan=Lan::createFromId($_GET['idLan']);
 	$lieu=$lan->getLieux();
-	$info=<<<XML
-<lan>
-	<nom>{$lan->getLanName()}</nom>
-	<date>{$lan->getLanDate()}</date>
-	<adresse>{$lan->getAdress()}</adresse>
-	<open>{$lan->isOpen()}</open>
-	<description>{$lan->getLanDescription()}</description>	
-	<lieu>
-		<nomSimple>{$lieu->getNomSimple()}</nomSimple>
-		<nomVille>{$lieu->getNomVille()}</nomVille>
-		<arrondissement>{$lieu->getArrondissement()}</arrondissement>
-		<code>{$lieu->getCodePostal()}</code>
-		<canton>{$lieu->getCanton()}</canton>
-		<slug>{$lieu->getSlug()}</slug>
-		<departement>{$lieu->getDepartement()}</departement>
-	</lieu>
-</lan>
-XML;
-			echo $xml.$info;
+	$info=array();
+	$info[]+=$lan->getLanName();
+	$info[]+=$lan->getLanDate();
+	$info[]+=$lan->getAdress();
+	$info[]+=$lan->isOpen();
+	$info[]+=$lan->getLanDescription();
+	/*$lieu=array();
+	$lieu+=$lieu->getNomSimple();
+	$lieu+=$lieu->getNomVille();
+	$lieu+=$lieu->getArrondissement();
+	$lieu+=$lieu->getCodePostal();
+	$lieu+=$lieu->getCanton();
+	$lieu+=$lieu->getSlug();	
+	$lieu+=$lieu->getDepartement();	
+	$info+=$lieu;*/
+		var_dump($info);
 }
 else
-	header('Location: ../erreur.php?erreur=un probléme est survenu');
+	echo "lol";//header('Location: ../erreur.php?erreur=un probléme est survenu');
