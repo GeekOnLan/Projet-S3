903aba3e20da708a024f59b7e89d682c87e1d4e9
Loic Schamber <loic.schamber@etudiant.univ-reims.fr>
2016-01-12 09:00:14 +0100

    Modification liste tournoi et ajout d'erreurs en cas de lan inexistante/ erreur dans les parametres

--- listeTournoi.php
+++ listeTournoi.php
@@ -4,59 +4,59 @@ require_once('includes/autoload.inc.php');
 
 if(isset($_REQUEST['idLan'])&&is_numeric($_REQUEST['idLan'])){
 
-$lan= Lan::createFromId($_REQUEST['idLan']);
+  try{
+    $lan= Lan::createFromId($_REQUEST['idLan']);
 
-$page = new GeekOnLanWebpage("GeekOnLan -".$lan->getLanName()." - Liste des tournois");
-$page->appendCssUrl("style/regular/listeTournoisMembre.css", "screen and (min-width: 680px");
+    $page = new GeekOnLanWebpage("GeekOnLan -".$lan->getLanName()." - Liste des tournois");
+    $page->appendCssUrl("style/regular/listeTournoisMembre.css", "screen and (min-width: 680px");
 
-$tournois=$lan->getTournoi();
+    $tournois=$lan->getTournoi();
 
-if(sizeof($tournois)==0){
-  $page->appendContent(<<<HTML
-<table>
-<tr>
-  <th>Aucun tournoi prévu pour cette LAN</th>
-</tr>
+    if(sizeof($tournois)==0){
+        $page->appendContent(<<<HTML
+              <table>
+              <tr>
+                <th>Aucun tournoi prévu pour cette LAN</th>
+              </tr>
 
-</table>
+              </table>
 HTML
-);
-}
-else{
-  $page->appendContent(<<<HTML
-  <table>
-  	<tr>
-  		<th>Nom</th>
-  		<th>Date et heure prévu</th>
-  		<th>Type Elimination</th>
-  		<th>Nombre d équipe</th>
-  		<th>Nombre de personnes par equipes</th>
-  	</tr>
-HTML
-);
+        );
+    }
+    else{
 
-  foreach ($tournois as $tournoi){
-    $page->appendContent(toString($tournoi));
+      $page->appendContent(<<<HTML
+              <table>
+              	<tr>
+              		<th>Nom</th>
+              		<th>Date et heure prévu</th>
+              		<th>Type Elimination</th>
+              		<th>Nombre d équipe</th>
+              		<th>Nombre de personnes par equipes</th>
+              	</tr>
+HTML
+      );
+
+        foreach ($tournois as $tournoi){
+          $page->appendContent(toString($tournoi));
+          $page->appendContent(<<<HTML
+
+              <tr>
+                <td colspan=5><a href="">Participer</a></td>
+              </tr>
+
+HTML
+          );
+        }
+      $page->appendContent("</table>");
+    }
+
+    echo $page->toHTML();
   }
-
-  $page->appendContent(<<<HTML
-
-  <tr>
-    <td colspan=5><a href="">Participer</a></td>
-  </tr>
-
-</table>"
-HTML
-);
-
-}
-
-
-
-
-
-echo $page->toHTML();
+  catch(Exception $e){
+    header('Location: message.php?message=Lan inexistante');
+  }
 }
 else{
-  header('Location: message.php?message=un problème est survenu');
+  header('Location: message.php?message=Un probleme est surve,u');
 }
